a674c2c9b5f68d6678a6ac3cf3bd7e211c55f730
jashvaru <jashvaru@gmail.com>
2024-07-20 18:44:53 +0530

    Correcting the code

--- demo.tf
+++ demo.tf
@@ -1,4 +1,4 @@
-resource "aws instance" "myec2" {
-  ami           = "ami-001"
-  instance_type = "t2.micro"
+resource "aws_instance" "myec2" {
+    ami = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
 }
