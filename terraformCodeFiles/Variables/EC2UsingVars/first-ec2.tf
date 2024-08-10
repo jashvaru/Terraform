762bc81f9e17315536ef258064ae77e01dea0d90
jashvaru <jashvaru@gmail.com>
2024-08-10 18:10:31 +0530

    Added comment in var definition example

--- terraformCodeFiles/Variables/EC2UsingVars/first-ec2.tf
+++ terraformCodeFiles/Variables/EC2UsingVars/first-ec2.tf
@@ -2,3 +2,7 @@ resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = "t2.micro"
 }
+
+/* 
+The value of the variable is picked from the dev.tfvars file
+*/
--- terraformCodeFiles/Variables/VarValueFromCLI/first-ec2.tf
+++ terraformCodeFiles/Variables/VarValueFromCLI/first-ec2.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.instance_type
 }
+
+/* 
+In CLI pass command as
+terraform plan -var="ami=a" -var="instance_type=b"
+*/
