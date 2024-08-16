fdd639d823dae7a86ba112ee289d29994d05cce7
jashvaru <jashvaru@gmail.com>
2024-08-16 14:58:02 +0530

    adding comment in the splat file

--- terraformCodeFiles/TerraformCommands/terraform-validate.tf
+++ terraformCodeFiles/TerraformCommands/terraform-validate.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myec2" {
   ami           = "ami-0ec0e125bb6c6e8ec"
   instance_type = "t2.micro"
-  sky           = "temp"
+  # sky           = "temp" //uncomment to run the example
 }
 
 /*  
