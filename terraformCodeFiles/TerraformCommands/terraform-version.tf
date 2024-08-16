7823d67374a729f5a9c0384c844a66ae6df93da6
jashvaru <jashvaru@gmail.com>
2024-08-16 16:23:33 +0530

    terraform version command

---
+++ terraformCodeFiles/TerraformCommands/terraform-version.tf
@@ -0,0 +1,10 @@
+/*  
+terraform version
+Terraform v1.9.2
+on windows_amd64
++ provider registry.terraform.io/hashicorp/aws v5.62.0
++ provider registry.terraform.io/hashicorp/local v2.5.1
+
+Your version of Terraform is out of date! The latest version
+is 1.9.3. You can update by downloading from https://www.terraform.io/downloads.html
+*/
