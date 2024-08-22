f8023509707deb39c7500303eacdd50c830d736a
jashvaru <jashvaru@gmail.com>
2024-08-22 10:01:25 +0530

    Terraform required provider block in module

--- terraformCodeFiles/TerraformModules/modules/ec2/main.tf
+++ terraformCodeFiles/TerraformModules/modules/ec2/main.tf
@@ -1,12 +1,25 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
 }
+
 
 resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.instance_type
 }
 
-variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
+
+
+
+/*  
+Step 1: Added varibles in the module file to increase its flexibility
+Step 2: instead of writing provider and its version in the ec2.tf file adding required block (terraform)
+in main.tf file
+*/
--- terraformCodeFiles/TerraformModules/teams/A/module-ec2.tf
+++ terraformCodeFiles/TerraformModules/teams/A/module-ec2.tf
@@ -1,6 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "ec2" {
   source        = "../../modules/ec2"
-  region        = "ap-south-1"
   ami           = "ami-1234"
   instance_type = "t2.micro"
 }
