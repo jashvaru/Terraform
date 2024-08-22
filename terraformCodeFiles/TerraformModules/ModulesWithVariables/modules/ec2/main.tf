105449c4271fc05bf6d92aa2b5961addbad372fc
jashvaru <jashvaru@gmail.com>
2024-08-22 09:45:15 +0530

    Example of terraform modules with variables

---
+++ terraformCodeFiles/TerraformModules/ModulesWithVariables/modules/ec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "myec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
+
+variable "region" {}
+variable "ami" {}
+variable "instance_type" {}
